7b3bd787e03510a7d928e50135ed128002e73ed7
Thaua <thauanlucascpl@gmail.com>
2022-09-26 00:59:22 -0300

    verify login header fix

--- src/services/isLogedService.tsx
+++ src/services/isLogedService.tsx
@@ -2,25 +2,26 @@ import axios from "axios";
 import { cepDataDto } from "../dto/cepData.dto";
 
 class IsLogedService {
-    async isLoged() {
-        let token = await localStorage.getItem("TOKEN");
-        return axios({
-          url: process.env.REACT_APP_API_URL + "token/verify",
-          method: "GET",
-          timeout: 5000,
-          headers: { Accept: "application/json", Authorization: "Bearer " + token },
-        })
-          .then(async (response) => {
-            if(!response.data.status){
-                await localStorage.removeItem("TOKEN")
-                return false
-            }
-            return true;
-          })
-          .catch((error) => {
-            return error;
-          });
-      }
+  async isLoged() {
+    let token = await localStorage.getItem("TOKEN");
+    return axios({
+      url: process.env.REACT_APP_API_URL + "token/verify",
+      method: "GET",
+      timeout: 5000,
+      headers: { Accept: "application/json", Authorization: "Bearer " + token },
+    })
+      .then(async (response) => {
+        if (response.data.status) {
+          console.log(response.data)
+          return true;
+        }
+        await localStorage.removeItem("TOKEN");
+        return false;
+      })
+      .catch((error) => {
+        return false;
+      });
+  }
 }
 
 const isLogedService = new IsLogedService();
